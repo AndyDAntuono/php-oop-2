0185e10305fb57e57aa852da352e1102313858ef
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-18 18:55:42 +0200

    [UPD] updated index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ class Product {
     public $category;
     public $productType;
 
-    public function __construct($title, $price, $image, $category, $productType) {
+    public function __construct($title, $price, $image, Category $category, $productType) {
         $this->title = $title;
         $this->price = $price;
         $this->image = $image;
@@ -39,5 +39,47 @@ $product2 = new Product("Cibo per Gatti", 9.99, "gioco-gatti.jpg", $categoryaCat
 
 var_dump($product1);
 var_dump($product2);
+
+// Le seguenti classi estendono la classe del prodotto
+class Food extends Product {
+    public $expiration;
+
+    public function __construct($title, $price, $image, Category $category, $expiration) {
+        parent::__construct($title, $price, $image, $category, "Cibo");
+        $this->expiration = $expiration;
+    }
+}
+
+class Game extends Product {
+    public $material;
+
+    public function __construct($title, $price, $image, Category $category, $material) {
+        parent::__construct($title, $price, $image, $category, "Gioco");
+        $this->material = $material;
+    }
+}
+
+class Kennel extends Product {
+    public $dimensions;
+
+    public function __construct($title, $price, $image, Category $category, $dimensions) {
+        parent::__construct($title, $price, $image, $category, "Cuccia");
+        $this->dimensions = $dimensions;
+    }
+}
+
+// Definizione delle categorie
+$categoryDogs = new Category("Cani", "icona-cani.png");
+$categoryCats = new Category("Gatti", "icona-gatti.png");
+
+// Creazione delle istanze
+$foodDogs = new Food("Cibo Premium Cani", 24.99, "cibo-premium-cani.jpg", $categoryDogs, "2025-12-31");
+$gameCats = new Game("Gioco Gatto Palla", 5.99, "gioco-palla-gatti.jpg", $categoryCats, "Plastica");
+$kennelDogs = new Kennel("Cuccia Grande", 49.99, "cuccia-grande.jpg", $categoryDogs, "90x60 cm");
+
+// Stampa dei risultati
+var_dump($foodDogs);
+var_dump($gameCats);
+var_dump($kennelDogs);
 
 ?>
