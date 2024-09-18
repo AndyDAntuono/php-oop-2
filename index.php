4b6f69faf4fb3440381f86adbdc3c1bf8949294a
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-18 16:51:49 +0200

    [UPD] updated index.php

--- index.php
+++ index.php
@@ -17,5 +17,27 @@ $categoryaCats = new Category("Gatti", "icon-gatti.png");
 var_dump($categoryDogs);
 var_dump($categoryaCats);
 
+// questa classe rappresenta un prodotto generico
+class Product {
+    public $title;
+    public $price;
+    public $image;
+    public $category;
+    public $productType;
+
+    public function __construct($title, $price, $image, $category, $productType) {
+        $this->title = $title;
+        $this->price = $price;
+        $this->image = $image;
+        $this->category = $category;
+        $this->productType = $productType;
+    }
+}
+
+$product1 = new Product("Cibo per Cani Premium", 19.99, "cibo-cani.jpg", $categoryDogs, "Cibo");
+$product2 = new Product("Cibo per Gatti", 9.99, "gioco-gatti.jpg", $categoryaCats, "Gioco");
+
+var_dump($product1);
+var_dump($product2);
 
 ?>
