bd8fcfad828a0a79c360c3916f904779daa3cc1b
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-18 20:09:24 +0200

    [UPD] updated index.php

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ L'e-commerce vende prodotti per animali.
 /*SOLUZIONE*/
 - per prima cosa creo le classi principali, ovvero Categoria (rappresenta le categorie Cani e Gatti), Prodotto (rappresenta un prodotto generico), Cibo, Gioco, Cuccia (queste tre rappresentano dei prodotti specifici), Shop (gestisce i prodotti)
 - creo una classe shop il cui scopo sarà gestire i prodotti e stampare le infomazioni in una struttura HTML, come una card.
+- modifico il file index.php in modo tale da aggiungere una porzone html il cui compito è far comparire a video le card rapprentanti i prodotti degli degli animali.
--- index.php
+++ index.php
@@ -112,3 +112,21 @@ $shop->addProduct($kennelDogs);
 $shop->showProducts();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Negozio di prodotti per animali</h1>
+    <div class="shop-container d-flex">
+        <?php
+        //mostra i prodotti del negozio
+        $shop->showProducts();
+        ?>
+    </div>
+</body>
+</html>
